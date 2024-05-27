c11a2a4270438de4e1a2a29b904152ee2d865c2f
leoleoyuuki <rm550373@fiap.com.br>
2024-05-27 03:52:06 -0300

    Update About page with links to GitHub and Curriculum

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,53 +1,37 @@
 "use client";
 import Link from "next/link";
-import { Typewriter } from "react-simple-typewriter";
+import { useTheme } from '../../utils/Theme';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { useTheme } from '../utils/Theme';
 import { IoDocumentTextSharp } from "react-icons/io5";
 
-export default function Home() {
+export default function About() {
   const { darkTheme } = useTheme();
 
   return (
-    <div className={`flex flex-col min-h-screen items-center pt-44 ${darkTheme ? 'text-white' : 'text-black bg-gray-100'}`}>
-      <Link href="about" className={`underline ${darkTheme ? 'text-blue-400 hover:text-blue-600' : 'text-blue-600 hover:text-blue-800'} transition-colors duration-200`} aria-label="About me">
-        Who Am I?
-      </Link>
-      <h1 className="text-6xl md:text-8xl font-extrabold tracking-wide pt-7 text-center">
-        Hi👋, I'm <span className="text-blue-600">Leo Yuuki</span>
+    <div className={`flex w-4/5 m-auto flex-col min-h-screen  pt-20 ${darkTheme ? 'text-white' : 'text-black bg-gray-100'}`}>
+
+      <h1 className="text-6xl md:text-8xl font-extrabold tracking-wide pt-7 text-left">
+        About Me
       </h1>
-      <p className="text-2xl md:text-4xl font-extrabold pt-9 text-center">
-        Full Stack Developer who likes to play with
+      <p className="text-2xl md:text-4xl font-extrabold pt-9 ">
+        Hello! I'm <span className="text-blue-600">Leonardo Yuuki Nakazone</span>
       </p>
-      <p className="text-2xl md:text-4xl font-extrabold tracking-wide text-blue-600 pt-2 text-center">
-        <Typewriter
-          loop={0}
-          words={[
-            "React",
-            "Next.Js",
-            "Firebase",
-            "React Native",
-            "Node.Js",
-            ".NET",
-            "Oracle PL/SQL",
-            "Python",
-            "ScikitLearn",
-            "Java",
-            "Spring",
-            "Javascript",
-          ]}
-        />
+      <p className="text-2xl md:text-4xl font-extrabold pt-9">
+        A Full Stack Developer & Data Scientist from São Paulo, SP
+      </p>
+      <p className="text-xl md:text-2xl pt-7  max-w-4xl">
+        I'm an 18-year-old tech enthusiast currently studying at FIAP. I have a passion for building interactive web applications and exploring data science. My expertise lies in technologies like React, Next.js, Firebase, SKLearn, Java and more. I love to learn new skills and continuously improve my knowledge.
       </p>
 
+
       <div className="mt-auto text-center pb-24">
-      <nav className="flex items-center justify-center gap-5 pb-4">
+        <nav className="flex items-center justify-center gap-5 pb-4">
           <Link href="https://github.com/leoleoyuuki" target="_blank" rel="noopener noreferrer" aria-label="GitHub profile">
             <FaGithub size={35} className="hover:text-blue-400 transition-colors duration-200" />
           </Link>
 
-          <Link href="https://github.com/leoleoyuuki/portfolio/blob/main/public/curriculo.pdf" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn profile">
-          <IoDocumentTextSharp size={35} className="hover:text-blue-400 transition-colors duration-200" />
-
+          <Link href="https://github.com/leoleoyuuki/portfolio/blob/main/public/curriculo.pdf" target="_blank" rel="noopener noreferrer" aria-label="Curriculum">
+            <IoDocumentTextSharp size={35} className="hover:text-blue-400 transition-colors duration-200" />
           </Link>
 
           <Link href="https://www.linkedin.com/in/leonardo-yuuki-nakazone-22a962295/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn profile">
@@ -69,8 +53,6 @@ export default function Home() {
             &nbsp;Github&nbsp;
           </Link>
         </p>
-
-        
       </div>
     </div>
   );
