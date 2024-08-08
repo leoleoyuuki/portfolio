a013b3b90e3481bfe023c41e651eb447e6393b2e
leoleoyuuki <rm550373@fiap.com.br>
2024-08-08 20:25:15 -0300

    chore: Update import path for ThemeProvider in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
-import { ThemeProvider } from "@/utils/Theme";
+import { ThemeProvider } from "../utils/Theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
