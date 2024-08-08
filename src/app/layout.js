a20223bb6281d8017b1f7bd49884441ceadcf62b
leoleoyuuki <rm550373@fiap.com.br>
2024-08-08 19:38:09 -0300

    SEO optimization

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,18 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Meu Portfólio - Leoyuuki</title>
+        <meta name="description" content="Leo Yuuki, desenvolvedor fullstack com foco em desenvolvimento web." />
+        <meta name="keywords" content="Next.js, Portfólio, Desenvolvedor Web, React, Dev, Fullstack" />
+        <meta name="author" content="leoleoyuuki" />
+        <link rel="canonical" href="https://leoyuuki.vercel.app/" />
+        <meta property="og:title" content="Meu Portfólio - Leoyuuki" />
+        <meta property="og:description" content="Leo Yuuki, desenvolvedor fullstack com foco em desenvolvimento web." />
+        <meta property="og:url" content="https://leoyuuki.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="../public/img/next.svg"/>
+      </Head>
       <ThemeProvider>
         <body className={inter.className}>
           <Header />
