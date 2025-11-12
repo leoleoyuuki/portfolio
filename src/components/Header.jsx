c4cf99f09e9ec7a037e6a2a0934ee73dee6a1964
leovercel1 <leovercel1@gmail.com>
2025-11-12 06:08:18 +0000

    Fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,19 +7,19 @@ import { useTheme } from '../utils/Theme';
 export default function Header() {
   const { darkTheme, toggleTheme } = useTheme();
 
-  return (
-    <div className={`flex justify-between p-5 px-14 ${darkTheme ? 'text-white'  : 'text-black bg-gray-100'}`}>
-      <Link href={'/'}>
-        <h1 translate="no" className="font-extrabold text-3xl tracking-wider">
-          <span className="text-blue-600">_l</span>eoyuuki
-        </h1>
-      </Link>
-      <button onClick={toggleTheme} aria-label="Toggle Dark Mode" className="transition-all focus:outline-none">
-        {darkTheme ? 
-          <MdOutlineLightMode className="transition-opacity" size={42} /> : 
-          <MdOutlineDarkMode className="transition-opacity" size={42} />
-        }
-      </button>
-    </div>
-  );
-}
+   return (
+     <div className={`flex justify-between items-center p-5 md:px-0 ${darkTheme ? 'text-white'  : 'text-black bg-gray-100'}`}>
+       <Link href={'/'}>
+         <h1 translate="no" className="font-extrabold text-3xl tracking-wider">
+           <span className="text-blue-600">_l</span>eoyuuki
+         </h1>
+       </Link>
+       <button onClick={toggleTheme} aria-label="Toggle Dark Mode" className="transition-all focus:outline-none">
+         {darkTheme ? 
+           <MdOutlineLightMode className="transition-opacity" size={42} /> : 
+           <MdOutlineDarkMode className="transition-opacity" size={42} />
+         }
+       </button>
+     </div>
+   );
+ }
